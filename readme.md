c88bfbea34bcc0c696c71228ff294f107a570581
Alex Townson <59401739+alextownson@users.noreply.github.com>
2023-01-10 13:48:54 -0500

    created project

--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
-#Trivia App 
+# Trivia App 
 
 The concept for this app came from this [React tutorial](https://www.youtube.com/watch?v=bMknfKXIFA8&t=5118s&ab_channel=freeCodeCamp.org). The tutorial outlined requirements for the app but the code is my own. 
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: 'assets/[hash][ext][query]'
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpe?g|gif|svg)$/,
+                type: 'asset'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
+        ]
+    },
+    devtool: 'source-map',
+    devServer: {
+        static: './dist',
+        open: true
+    }
+}
