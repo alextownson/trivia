161267cfa13ae6ff3bcdae03b69df82890d39542
Alex Townson <59401739+alextownson@users.noreply.github.com>
2023-01-12 16:12:43 -0500

    first attempt

---
+++ src/dataExample.js
@@ -0,0 +1,42 @@
+[
+    { 
+        category: "Entertainment: Video Games",
+        correct_answer: "Mortal Kombat",
+        difficulty: "easy",
+        incorrect_answers: ['Duke Nukem', 'Resident Evil', 'Doom'],
+        question: "The most graphically violent game to precede the creation of the ESRB (Entertainment Software Rating Board) was...",
+        type: "multiple"
+    },
+    {
+        category: "Entertainment: Video Games",
+        correct_answer: "Townley",
+        difficulty: "easy",
+        incorrect_answers: ['De Santos', 'Watson', 'Simpson'],
+        question: "In Grand Theft Auto V, what was Michael De Santa&#039;s former surname?",
+        type: "multiple",
+    },
+    {
+        category: "Entertainment: Video Games",
+        correct_answer: "The Zin Empire",
+        difficulty: "easy",
+        incorrect_answers: ['The Brotherhood', 'The Ronin', 'The Sons of Samedi'],
+        question: "Which of these is NOT the name of a rival gang in the video game Saint&#039;s Row 2?",
+        type: "multiple"
+    },
+    {
+        category: "Entertainment: Video Games",
+        correct_answer: "Game Boy Color",
+        difficulty: "easy",
+        incorrect_answers: ['Game Boy Advance', 'Game Boy Micro', 'Game Boy Advance SP'],
+        question: "Which Game Boy from the Game Boy series of portable video game consoles was released first?",
+        type: "multiple"
+    },
+    {
+        category: "Entertainment: Video Games",
+        correct_answer: "The HECU",
+        difficulty: "easy",
+        incorrect_answers: ['Navy Seals', 'The Combine', 'The Marines'],
+        question: "In the Video Game, Half-life, what type of US military force starts clearing out the Black Mesa Research Facility?",
+        type: "multiple"
+    }
+]
