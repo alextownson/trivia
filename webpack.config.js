4084204f004e292e3f3465039d74bd16cba36dde
Alex Townson <59401739+alextownson@users.noreply.github.com>
2023-01-13 20:25:57 -0500

    build

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
