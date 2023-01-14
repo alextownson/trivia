d16df3ddd67cdaa454d1fbca3304cde8fe2bc261
Alex Townson <59401739+alextownson@users.noreply.github.com>
2023-01-14 15:31:12 -0500

    fixed favicon path

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,8 @@ module.exports = {
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            template: './src/index.html'
+            template: './src/index.html',
+            favicon: './src/assets/icon.ico'
         }),
         new MiniCssExtractPlugin()
     ],
